3dbf3e7482d983d7e9d1eefe8dbfd49165d13137
DotHrishi <kalihrishikesh@gmail.com>
2025-03-31 10:29:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-// // import generateName from "sillyname"
-// // var sillyName = generateName();
+// import generateName from "sillyname"
+// var sillyName = generateName();
 
-// // console.log('My name is '+ sillyName);
+// console.log('My name is '+ sillyName);
 
-// // import generateStupidName from "sillyname";
+// import generateStupidName from "sillyname";
 
-// // var newname=generateStupidName();
+// var newname=generateStupidName();
 
-// // console.log("I am "+ newname);
+// console.log("I am "+ newname);
 
-// import inquirer from "inquirer";
-// import qr from "qr-image";
-// import fs from "fs";
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
 
-// inquirer.prompt([
-//     {"message":"Enter url to convert in QR code",
-//        "name":"URL",},
-// ])
-// .then((answers)=>{
-//     const url=answers.URL;
-//     var qr_svg = qr.image(url);
-//     qr_svg.pipe(fs.createWriteStream('qrimg.png'));
-// })
-// .catch((error) => {
-//     if (error.isTtyError) {
-//       // Prompt couldn't be rendered in the current environment
-//     } else {
-//       // Something else went wrong
-//     }
-//   });
-
-import f1s from "f1-stats"
+inquirer.prompt([
+    {"message":"Enter url to convert in QR code",
+       "name":"URL",},
+])
+.then((answers)=>{
+    const url=answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qrimg.png'));
+})
+.catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
